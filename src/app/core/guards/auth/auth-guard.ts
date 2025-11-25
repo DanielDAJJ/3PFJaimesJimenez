54fbd720f01577514bc0f81c332b0b445213f7d7
DanielDAJJ <141734343+DanielDAJJ@users.noreply.github.com>
2025-11-25 13:40:25 -0500

    auth-guard update

--- src/app/core/guards/auth/auth-guard.ts
+++ src/app/core/guards/auth/auth-guard.ts
@@ -1,18 +1,15 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { UserService } from '../../services/users/user';
+import { map } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const userService = inject(UserService);
   const router = inject(Router);
 
-  if(!userService.isLoggedIn()){
-    //Si no funciona con currentUserLoginOn$, usar otro la verificacion con el metodo login()
-    router.navigate(['/login']);
-    return false;
-  }
-
-
-
-  return true;
+  return userService.currentUserLoginOn$.pipe(
+    map(isLoggedIn => {
+      return isLoggedIn ? true : router.navigate(['/login']), false;
+    })
+  );
 };
