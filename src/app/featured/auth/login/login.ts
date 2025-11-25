65330fedfafc494c27b719e5d5c1da680babd8fd
DanielDAJJ <141734343+DanielDAJJ@users.noreply.github.com>
2025-11-25 10:08:53 -0500

    Preentrega 3 guards

--- src/app/featured/auth/login/login.ts
+++ src/app/featured/auth/login/login.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../../core/services/users/user';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrl: './login.css',
 })
 export class Login {
-
+  loginForm : FormGroup;
+  constructor(private userService: UserService, private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+  login(){
+    if(!this.loginForm.valid){
+      alert('Formulario no válido');
+      return;
+    }
+    this.userService.login(this.loginForm.value).subscribe(users => {
+      if(users.length === 0 || !users){
+        alert('Credenciales incorrectas');
+      } else {
+        this.router.navigate(['/dashboard']);
+      }
+    })
+  }
 }
