026dc509583527caa5b13f3086e4398c124849a9
DanielDAJJ <141734343+DanielDAJJ@users.noreply.github.com>
2025-12-08 15:06:52 -0500

    inicio unir con servicio

---
+++ src/app/core/services/API/api.ts
@@ -0,0 +1,10 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+ 
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Api {
+  
+}
