3941cdf26d7173ec98011021a107a8fc2b767920
DanielDAJJ <141734343+DanielDAJJ@users.noreply.github.com>
2025-12-08 21:21:49 -0500

    ApiService actualizado

--- src/app/core/services/API/api.ts
+++ src/app/core/services/API/api.ts
@@ -1,10 +1,34 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
  
 
 @Injectable({
   providedIn: 'root',
 })
-export class Api {
-  
+export class ApiService {
+  private readonly APIUrl= "https://6932f5d1e5a9e342d271726b.mockapi.io/api/v1/";
+
+  constructor(private http: HttpClient) {}
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    let errorMessage = 'An unknown error occurred.';
+
+    if(error.error instanceof ErrorEvent){
+      errorMessage = `Error: ${error.error.message}`;
+    }
+    else if (error.status === 0) {
+      errorMessage = 'Network error: Please check your internet connection.';
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(()=> new Error(errorMessage));
+  }
+  get<T>(endpoint: string): Observable<T> {
+    return this.http.get<T>(`$`)
+  }
 }
+
+
